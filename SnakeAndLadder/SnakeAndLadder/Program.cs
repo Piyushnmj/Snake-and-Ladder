52c4a6abd9422e2dd5ddd03cafd7de8faa69eb22
Piyushnmj <piyushnmj@yahoo.com>
2022-11-02 15:24:32 +0530

    Rolling a Dice

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -7,11 +7,15 @@
             Console.WriteLine("Welcome to Snake and Ladder Game !!");
 
             int PlayerPosition = 0;
+            int diceRoll;
+            Random random = new Random();
 
-            Console.WriteLine("\nPress w to Play the game");
+            Console.WriteLine("\nEnter w to Play the game");
             string play = Console.ReadLine();
+            Console.WriteLine($"\nPlayer is at position {PlayerPosition}");
 
-            Console.WriteLine($"\nPlayer is at {PlayerPosition} position.");
+            diceRoll = random.Next(1, 6);
+            Console.WriteLine($"\nPlayer rolled number {diceRoll}");
         }
     }
 }
